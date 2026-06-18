3c83dd9891432e8ecc6d1f0310733bf494c1d018
ster55 <rm100423@estudante.fieb.edu.br>
2026-06-18 10:34:31 +0000

    Criação fo projeto Spring bootgit

---
+++ sgt/src/test/java/com/itb/sistema/sgt/SgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.sistema.sgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
